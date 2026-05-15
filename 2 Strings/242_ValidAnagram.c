65104ff5db3d19ab9c81c99713dc487609f9b62b
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-05-15 15:45:05 +0200

    anagram

--- 2 Strings/242_ValidAnagram.c
+++ 2 Strings/242_ValidAnagram.c
@@ -3,5 +3,32 @@
 
 bool isAnagram(char* s, char* t) {
     
+    int liczydloS[26] = {0};
+
+    size_t i = 0;
+    while(s[i]!=0)
+    {
+        int index = s[i] - 'a';
+        liczydloS[index] += 1;
+        i+=1;
+    }
+
+    int liczydloT[26] = {0};
+
+    size_t j = 0;
+    while(t[j]!=0)
+    {
+        int index = t[j] - 'a';
+        liczydloT[index] += 1;
+        j+=1;
+    }
+
+    for(size_t k = 0; k<26; k++)
+    {
+        if(liczydloS[k]!=liczydloT[k]) {return false;}
+    }
+
+    return true;
+
 }
 
